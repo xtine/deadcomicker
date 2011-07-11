5632d549f3ec04fb640d76ae8230e7a4e85b690f
xtine <piroko@gmail.com>
2011-07-11 01:41:19 -0700

    Change List Display Links (id first, title second and clickable)

--- comics/admin.py
+++ comics/admin.py
@@ -2,6 +2,7 @@ from comics.models import Comic
 from django.contrib import admin
 
 class ComicAdmin(admin.ModelAdmin):
-    list_display = ('title', 'pub_date')
+    list_display = ('id', 'title')
+    list_display_links = ['title']
 
 admin.site.register(Comic, ComicAdmin)

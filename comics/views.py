2c26fa3044d9762c1cafceb4b41d888a44e3c8c1
xtine <piroko@gmail.com>
2011-07-11 01:41:55 -0700

    sort by newer id

--- comics/views.py
+++ comics/views.py
@@ -6,6 +6,6 @@ from django.template import RequestContext
 from comics.models import Comic
 
 def index(request):
-    comics = Comic.objects.all().order_by('pub_date')
+    comics = Comic.objects.all().order_by('-id')
     context = RequestContext(request)
     return render_to_response('index.html', {'comics' : comics}, context_instance=context)

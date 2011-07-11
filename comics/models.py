cd9b2a795db2bc2c985d3ed1d24ff5346462108e
xtine <piroko@gmail.com>
2011-07-10 22:24:47 -0700

    Comics model change for upload image field

--- comics/models.py
+++ comics/models.py
@@ -4,6 +4,7 @@ from filebrowser.fields import FileBrowseField
 class Comic(models.Model):
     title = models.CharField(max_length=300)
     entry = models.TextField(blank=True)
-    image = FileBrowseField("Image", max_length=200, directory="images/", extensions=[".jpg"], blank=True, null=True)
+    # image = FileBrowseField("Image", max_length=200, directory="images/", extensions=[".jpg"], blank=True, null=True)
+    image = models.FileField(upload_to='images', max_length=500)
     pub_date = models.DateTimeField('date published')
     

328447a971a66b89136154868b46adaa87a5099b
xtine <piroko@gmail.com>
2011-07-11 01:26:28 -0700

    Auto-populating created/updated datetime objects

--- comics/models.py
+++ comics/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-from filebrowser.fields import FileBrowseField
+import datetime
 
 class Comic(models.Model):
     title = models.CharField(max_length=300)
     entry = models.TextField(blank=True)
-    # image = FileBrowseField("Image", max_length=200, directory="images/", extensions=[".jpg"], blank=True, null=True)
-    image = models.FileField(upload_to='images', max_length=500)
-    pub_date = models.DateTimeField('date published')
+    image = models.FileField(upload_to='images', max_length=500, blank=True)
+    created = models.DateField(editable=False)
+    updated = models.DateTimeField(editable=False)
+    
+    def save(self):
+        if not self.id:
+            self.created = datetime.date.today()
+        self.updated = datetime.datetime.today()
+        super(Comic, self).save()
     

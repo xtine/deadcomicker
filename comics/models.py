36c7b10ee87bf5f5a6f463b07777a1f89549527a
xtine <piroko@gmail.com>
2011-07-12 00:02:42 -0700

    set default self return

--- comics/models.py
+++ comics/models.py
@@ -4,7 +4,7 @@ import datetime
 class Comic(models.Model):
     title = models.CharField(max_length=300)
     entry = models.TextField(blank=True)
-    image = models.FileField(upload_to='images', max_length=500, blank=True)
+    image = models.FileField(upload_to='comics', max_length=500, blank=True)
     created = models.DateField(editable=False)
     updated = models.DateTimeField(editable=False)
     
@@ -13,4 +13,7 @@ class Comic(models.Model):
             self.created = datetime.date.today()
         self.updated = datetime.datetime.today()
         super(Comic, self).save()
+    
+    def __unicode__(self):
+        return self.title
     

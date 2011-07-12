57af895b3a52f1b0339f6ec24c3a107cfdee2aed
xtine <piroko@gmail.com>
2011-07-12 00:01:37 -0700

    Comic display and navigation contexts

--- comics/views.py
+++ comics/views.py
@@ -1,11 +1,58 @@
-from django.shortcuts import get_object_or_404, render_to_response
-from django.http import HttpResponseRedirect, HttpResponse, Http404
-
-from django.template import RequestContext
-
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import Http404, HttpResponseRedirect
+from django.template import RequestContext, Context
 from comics.models import Comic
 
+import random
+
+# function to return context
+# for comic navigation links
+def nav(comic_id):
+    # grab previous comic
+    if comic_id > 1:
+        prev = comic_id - 1
+    else:
+        prev = None
+    # grab last comic
+    if comic_id == Comic.objects.count():
+        last = None
+    else:
+        last = Comic.objects.count()
+    # grab next comic
+    if comic_id != last:
+        next = comic_id + 1
+    else:
+        next = None
+                
+    return Context({'last' : last, 'prev' : prev, 'next' : next})
+
+# front page
 def index(request):
-    comics = Comic.objects.all().order_by('-id')
+    comic = Comic.objects.all().order_by('-id')[0]
+    
     context = RequestContext(request)
-    return render_to_response('index.html', {'comics' : comics}, context_instance=context)
+    return render_to_response('comic.html', {'comic' : comic, 'nav' : nav(comic.id)}, context_instance=context)
+
+# comic page
+def comic(request, comic_id):
+    try:
+        c = Comic.objects.get(pk=comic_id)
+    except:
+        raise Http404
+        
+    context = RequestContext(request)
+    return render_to_response('comic.html', {'comic' : c, 'nav' : nav(int(comic_id))}, context_instance=context)
+
+# random comic
+def random_comic(request):
+    random_comic_id = str(random.randint(1, Comic.objects.count()))
+    
+    return HttpResponseRedirect('/comic/' + random_comic_id)
+
+# archive
+def archive(request):
+    c = Comic.objects.all().values('id', 'title')
+    
+    context = RequestContext(request)
+    return render_to_response('archive.html', {'comics' : c})
+    
